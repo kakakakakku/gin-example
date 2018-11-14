7c725c2aefbb41c34f6aef66577c5c0f41e724ff
kakakakakku <y.yoshida22@gmail.com>
2018-11-14 23:33:08 +0900

    Updated main.go

--- main.go
+++ main.go
@@ -4,10 +4,20 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	r.GET("/users/:name", func(c *gin.Context) {
+		name := c.Params.ByName("name")
+		c.JSON(200, gin.H{
+			"name":    name,
+			"message": "hello!",
+		})
+	})
+
 	r.Run()
 }
